a98a937e217a57c3678700a39a9ac96554542f10
JakubD02 <114111975+JakubD02@users.noreply.github.com>
2024-11-01 09:39:22 +0100

    Added Note tests

---
+++ tests/test_model/test_note.gd
@@ -0,0 +1,23 @@
+extends Node
+class_name TestNote
+
+# Test 1 - Appropriate properties
+funct test_note_initialization():
+    var note = Note.new(Note.NoteType.QUARTER, Note.ObjectType.NOTE)
+    var rest = Note.new(Note.NoteType.QUARTER, Note.ObjectType.REST)
+    assert(note.isPause() == false, "The note should be of type Note, but it is recognized as a pause")
+    assert(rest.isPause() == true, "The note shoulde be of type Rest, but it isn't recognized as a pause")
+
+# test 2
+
+
+# test 3
+
+
+# test 4
+
+func run_tests():
+    test_note_initialization()
+
+func _ready():
+    run_tests()
