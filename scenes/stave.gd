82110462ac6b6c362b60d6403fba3c8759dadc0a
kicerKs <osk.ch18@gmail.com>
2024-11-05 12:20:32 +0100

    set up measure with numbers from script

--- scenes/stave.gd
+++ scenes/stave.gd
@@ -8,6 +8,8 @@ func _ready() -> void:
 	var measure_up = 4
 	var measure_down = 4
 	$Barline.position.x = 150 + (note_size + space_between_notes) * (measure_up * (16/measure_down)) + 10
+	$MeasureDownLabel.text = str(measure_down)
+	$MeasureUpLabel.text = str(measure_up)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
