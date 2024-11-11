afa2c9ef71015cb882de4c78a87e01377df3d438
kicerKs <osk.ch18@gmail.com>
2024-11-11 12:36:03 +0100

    change MidiImport/Export to singletons; add global singleton, holding current track

--- scenes/editor.gd
+++ scenes/editor.gd
@@ -2,10 +2,10 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var fur_elise_track = MidiImport.new("./demos/furEliseDemo.mid").get_track()
+	MidiImport.load_file("./demos/furEliseDemo.mid")
+	var fur_elise_track = Global.current_track
 	print(fur_elise_track)
-	MidiExport.new("./demos/furEliseDemoExport.mid", fur_elise_track)
-
+	MidiExport.save_file("./demos/furEliseDemoExport.mid")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
