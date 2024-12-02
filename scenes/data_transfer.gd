7a0bf60fb522c71991c150ee4843927b27a8eeca
JakubD02 <114111975+JakubD02@users.noreply.github.com>
2024-12-02 12:36:49 +0100

    Added top navigation menu
    before adding the container and icons

---
+++ scenes/data_transfer.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_import_button_pressed() -> void:
+	print("test321")
+	#pass # Replace with function body.
+
+
+func _on_export_button_pressed() -> void:
+	print("test123")
+	#pass # Replace with function body.
