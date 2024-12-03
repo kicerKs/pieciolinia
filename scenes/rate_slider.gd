fce26e4f9577153a6b99a765c7aecb5e05efc1c2
JakubD02 <114111975+JakubD02@users.noreply.github.com>
2024-12-03 11:04:39 +0100

    Added exit button and logic

---
+++ scenes/rate_slider.gd
@@ -0,0 +1,13 @@
+extends HSlider
+#
+#@onready var melody = preload("res://Model/Melody.gd").new()
+#
+#func _ready() -> void:
+	#value = melody.rate
+#
+	#connect("value_changed","on_value_changed")
+	##connect("value_changed", self, "_on_value_changed")
+#
+#func _on_value_changed(value: float) -> void:
+	#melody.rate = int(value)
+	#print(melody.rate)
