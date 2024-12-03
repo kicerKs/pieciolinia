704d65d529fe79da3a840a57461e1a35936f15c0
JakubD02 <114111975+JakubD02@users.noreply.github.com>
2024-12-03 09:18:10 +0100

    Arragement of buttons upper menu.

--- scenes/data_transfer.gd
+++ scenes/data_transfer.gd
@@ -9,13 +9,3 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
-
-func _on_import_button_pressed() -> void:
-	print("test321")
-	#pass # Replace with function body.
-
-
-func _on_export_button_pressed() -> void:
-	print("test123")
-	#pass # Replace with function body.
