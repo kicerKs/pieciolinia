5461bed530cc402f8a8871b38f30d2e876daa861
JakubD02 <114111975+JakubD02@users.noreply.github.com>
2024-12-04 15:49:52 +0100

    Added volume to slider

--- scenes/volume_slider.gd
+++ scenes/volume_slider.gd
@@ -5,9 +5,10 @@ var melody: Node = null
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	melody = get_node("/root/Melody")
-	value = melody.rate
+	value = melody.volume
 	value_changed.connect(_on_value_changed)
 
 func _on_value_changed(value: float) -> void:
 	if melody:
-		melody.rate = int(value)
+		melody.volume = int(value)
+		print(melody.volume)
