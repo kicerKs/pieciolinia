912091d004aab37fd95126ba46a703862a3af31c
JakubD02 <114111975+JakubD02@users.noreply.github.com>
2024-12-05 10:56:03 +0100

    Added explorer in save/open

--- scenes/data_transfer.gd
+++ scenes/data_transfer.gd
@@ -1,33 +1,20 @@
 extends Node2D
 
 @onready var popup = $PopUpExit
+@onready var MIDI_save = $MIDI_Save
+@onready var MIDI_load = $MIDI_Load
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	MIDI_save.current_dir = "/"
+	pass 
 
 
- #Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
 
 func _on_exit_button_button_down() -> void:
 	popup.show()
-	#get_tree().quit()
-
-
-#func _on_exit_approved_button_down() -> void:
-	#get_tree().quit()
-
-
-#func _on_window_close_requested() -> void:
-	#popup.hide()
-
-
-#func _on_pop_up_exit_close_requested() -> void:
-	#popup.hide()
-	#pass # Replace with function body.
 
 
 func _on_pop_up_exit_close_requested() -> void:
@@ -37,5 +24,23 @@ func _on_pop_up_exit_close_requested() -> void:
 func _on_button_approved_button_down() -> void:
 	get_tree().quit()
 
+
 func _on_button_discard_button_down() -> void:
 	popup.hide()
+
+
+func _on_midi_save_file_selected(path: String) -> void:
+	var save_file = FileAccess.open(path, FileAccess.WRITE)
+	#save_file.store_line("Example text123")
+
+func _on_midi_load_file_selected(path: String) -> void:
+	var load_file = FileAccess.open(path, FileAccess.READ)
+	#if FileAccess.file_exists(path): i sprawdz czy jest rozna od null
+		#print(load_file.get_line())
+
+
+func _on_import_button_pressed() -> void: # otwórz/open
+	MIDI_load.visible = true
+
+func _on_export_button_pressed() -> void: # zapisz/save
+	MIDI_save.visible = true
