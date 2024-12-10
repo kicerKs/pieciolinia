91cff99cb1db7832b826195b768d64624230d602
kicerKs <osk.ch18@gmail.com>
2024-12-10 01:35:34 +0100

    remade file structure for more clarity

--- globals/global.gd
+++ globals/global.gd
@@ -1,6 +1,5 @@
 extends Node
 
-#var current_track: TrackOld = TrackOld.new(0,0,0,0,Instruments.ACOUSTIC_BASS,[])
 var stave_length: int = 0
 var current_viewing_track = 0
 var max_track = 0
---
+++ scenes/main/editor.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+@export var stave: PackedScene
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+
+func initialize_editor() -> void:
+	for stave in get_tree().get_nodes_in_group("staves"):
+		stave.queue_free()
+	for i in range(len(Melody.tracks)):
+		var new_stave = stave.instantiate()
+		add_child(new_stave)
+		new_stave.setup_stave(i)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
