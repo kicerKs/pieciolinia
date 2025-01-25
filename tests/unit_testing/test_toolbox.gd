bd823fa9defa68ead8e0891c2ccd91fe82d5f426
JakubD02 <114111975+JakubD02@users.noreply.github.com>
2025-01-25 22:26:57 +0100

    Added rate tests
    Includes improved volume slider test

--- tests/unit_testing/test_toolbox.gd
+++ tests/unit_testing/test_toolbox.gd
@@ -2,6 +2,10 @@ extends "res://addons/gut/test.gd"
 
 var test_root: Node = null
 
+func _cleanup(mainScene):
+	if mainScene and mainScene.is_inside_tree():
+		mainScene.queue_free()
+		
 func test_volume_slider():
 	var mainScene = load("res://scenes/main/main.tscn").instantiate()
 	await wait_until(func ():
@@ -11,7 +15,23 @@ func test_volume_slider():
 	mainScene.name = "Main"
 	var slider = get_node("/root/Main/GUI/DataTransfer/Control/MarginContainer/HBoxContainer/HBoxVolume/VolumeSlider")
 	
-	assert(slider != null, "VolumeSlider not found!")
+	assert_not_null(slider != null, "VolumeSlider not found!")
 	slider.value = 60
-	assert(slider.value == 60, "Value of slider is wrong!")
+	assert_eq(slider.value, 60, "Value of slider is wrong!")
 	print("Everything is ok")
+	_cleanup(mainScene)
+	
+func test_rate_slider():
+	var mainScene = load("res://scenes/main/main.tscn").instantiate()
+	await wait_until(func ():
+		get_parent().get_parent().add_child(mainScene)
+		return mainScene.is_inside_tree()
+	, 5)
+	mainScene.name = "Main"
+	var slider = get_node("/root/Main/GUI/DataTransfer/Control/MarginContainer/HBoxContainer/HBoxContainer/RateSlider")
+	
+	assert_not_null(slider != null, "RateSlider not found!")
+	slider.value = 60
+	assert_eq(slider.value, 60, "Value of slider is wrong!")
+	print("Everything is ok")
+	_cleanup(mainScene)
